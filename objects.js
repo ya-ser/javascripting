e8f34ab2ab08822106d172e1abf778bc616cc13d
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-11-05 23:49:18 +0000

    created object and printed out its properties

---
+++ objects.js
@@ -0,0 +1,11 @@
+/*const pizza = {  
+  toppings: ['cheese', 'sauce', 'pepperoni'],  
+  crust: 'deep dish',  
+  serves: 2  
+}
+console.log(pizza)
+*/
+const food = {  
+  types: 'only pizza'  
+}  
+console.log(food.types)
